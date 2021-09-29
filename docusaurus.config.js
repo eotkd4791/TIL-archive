c65c4b151e5094ac3ea776e687ccdcefc91f7098
eotkd4791 <eotkd4791@gmail.com>
2021-09-30 05:52:48 +0900

    Update configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,8 +12,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'eotkd4791', // Usually your GitHub org/user name.
+    projectName: 'TIL-archive', // Usually your repo name.
 
     presets: [
       [
@@ -21,14 +21,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         /** @type {import('@docusaurus/preset-classic').Options} */
         ({
           docs: {
-            sidebarPath: require.resolve('./sidebars.js'),
+            sidebarPath: require.resolve('./sidebars.js')
             // Please change this to your repo.
-            editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/'
+            // editUrl: 'https://github.com/eotkd4791/til-archive/'
           },
           blog: {
-            showReadingTime: true,
+            showReadingTime: true
             // Please change this to your repo.
-            editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/blog/'
+            // editUrl: 'https://github.com/eotkd4791/til-archive/'
           },
           theme: {
             customCss: require.resolve('./src/css/custom.css')
@@ -41,10 +41,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
         navbar: {
-          title: 'My Site',
+          title: 'Today Daesang Learned',
           logo: {
-            alt: 'My Site Logo',
-            src: 'img/logo.svg'
+            alt: 'Today Daesang Learned logo',
+            src: 'img/stef.png'
           },
           items: [
             {
@@ -53,58 +53,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
               position: 'left',
               label: 'Tutorial'
             },
-            { to: '/blog', label: 'Blog', position: 'left' },
+            // { to: '/blog', label: 'Blog', position: 'left' },
             {
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/eotkd4791',
               label: 'GitHub',
               position: 'right'
             }
           ]
-        },
-        footer: {
-          style: 'dark',
-          links: [
-            {
-              title: 'Docs',
-              items: [
-                {
-                  label: 'Tutorial',
-                  to: '/docs/intro'
-                }
-              ]
-            },
-            {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Stack Overflow',
-                  href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-                },
-                {
-                  label: 'Discord',
-                  href: 'https://discordapp.com/invite/docusaurus'
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/docusaurus'
-                }
-              ]
-            },
-            {
-              title: 'More',
-              items: [
-                {
-                  label: 'Blog',
-                  to: '/blog'
-                },
-                {
-                  label: 'GitHub',
-                  href: 'https://github.com/facebook/docusaurus'
-                }
-              ]
-            }
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
         },
         prism: {
           theme: lightCodeTheme,
